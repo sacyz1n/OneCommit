609fd5c5ea4a5bb641061cc36c53c9e38d801068
sacyz1n <sacyz1n@naver.com>
2020-10-28 14:13:59 +0900

    level 1, K번째 수

--- algorithm/algorithm.cpp
+++ algorithm/algorithm.cpp
@@ -3,9 +3,14 @@
 
 #include <iostream>
 #include "lvl_sol_1.h"
+#include "lvl_sol_2.h"
 int main()
 {
-	for (auto number : sol_1::solution({ 1,2,3,4,5,6,7 }))
+	//for (auto number : sol_1::solution({ 1,2,3,4,5,6,7 }))
+	//	std::cout << number << std::endl;
+
+
+	for (auto number : sol_2::solution({ 1, 5, 2, 6, 3, 7, 4 }, { { 2, 5, 3 }, { 4, 4, 1 }, { 1, 7, 3 } }))
 		std::cout << number << std::endl;
 }
 
