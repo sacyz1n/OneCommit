3bfce0dcda05a42123a02231eff3f35381cd16ea
sacyz1n <sacyz1n@naver.com>
2020-10-29 23:43:21 +0900

    level 1, 완주하지 못한 선수

--- algorithm/algorithm.cpp
+++ algorithm/algorithm.cpp
@@ -4,13 +4,16 @@
 #include <iostream>
 #include "lvl_sol_1.h"
 #include "lvl_sol_2.h"
+#include "lvl_sol_3.h"
 int main()
 {
 	//for (auto number : sol_1::solution({ 1,2,3,4,5,6,7 }))
 	//	std::cout << number << std::endl;
 
-
 	for (auto number : sol_2::solution({ 1, 5, 2, 6, 3, 7, 4 }, { { 2, 5, 3 }, { 4, 4, 1 }, { 1, 7, 3 } }))
 		std::cout << number << std::endl;
+
+	// sol 3
+	std::cout << sol_3::solution({ "mislav", "stanko", "mislav", "ana" }, { "stanko", "ana", "mislav" }) << std::endl;
 }
 
